e7cb9bd51c7a4d172b898f5212bb1e660f8114e4
hannesthaler <02thahan@rgtfo-me.it>
2020-04-16 10:46:12 +0200

    Titel schreiben

--- Test4AEL/Test4AEL/Program.cs
+++ Test4AEL/Test4AEL/Program.cs
@@ -7,9 +7,25 @@ namespace Test4AEL
 {
     class Program
     {
+        //**Konvertierung in Zahlensysteme
         static void Main(string[] args)
         {
+           
+            Titel("Dies ist ein TESt");
             Console.ReadKey();
+        }
+        static void Titel(string titel)
+        {
+            Console.Clear();
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(titel);
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
         }
     }
 }
